2643622f89407b18556597372d068f6616d0e0c7
Ronaldo Salmon <ronopedia@gmail.com>
2021-10-23 23:34:58 -0500

    Edited scripts.js file

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
 /* Add your JavaScript to this file */
 function subscription(){ 
-
-
-
-
+    var butn = document.getElementsByTagName("button")[0];
+    var regexp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    butn.addEventListener("click",function(e){
+        var email= document.getElementById("email").value;
+        e.preventDefault();
+        if(email.match(regexp)){
+            document.getElementsByClassName("message")[0].innerHTML = `Thank you! Your email address ${email} has been added to our mailing list`;
+        }
+        else{
+            document.getElementsByClassName("message")[0].innerHTML = "Please Input Valid Email Address";
+        }
+    });
 }
 window.onload=subscription;
